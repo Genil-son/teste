15a4e048965e961ae52776657fbf9bda3d095a58
Genil-son <143018894+Genil-son@users.noreply.github.com>
2023-08-23 22:29:53 +0100

    subir os arquivos

---
+++ ind.js
@@ -0,0 +1,8 @@
+var menu = document.querySelector('nav ul');
+var menuBar = document.querySelector('nav .menu-icon');
+var iconMenu = document.querySelector('nav .menu-icon svg');
+
+menuBar.addEventListener('click',function(){
+menu.classList.toggle('active')
+alert('Menu activado pressione OK ------------> GENILSON')
+});
